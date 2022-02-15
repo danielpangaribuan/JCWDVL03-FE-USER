a2574dfb2b3c9b16569372f9d843ea3c9dcf337e
Daniel <dpangaribuan40@gmail.com>
2022-02-15 21:24:07 +0700

    Update Add CSS

--- fe-user/src/index.js
+++ fe-user/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/login_overlay.css";
+import "./assets/css/style6.css";
+import "./assets/css/shop.css";
+import "./assets/css/owl.carousel.css";
+import "./assets/css/owl.theme.css";
+import "./assets/css/style.css";
+import "./assets/css/fontawesome-all.css";
 
 // main component
 import Main from "./Main";
