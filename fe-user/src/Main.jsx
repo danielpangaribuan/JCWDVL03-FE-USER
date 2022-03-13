b0adcbbc950985285990609c1a3caed3d4cc655e
Daniel <dpangaribuan40@gmail.com>
2022-03-13 15:02:10 +0700

    update jquery menu dropdown

--- fe-user/src/Main.jsx
+++ fe-user/src/Main.jsx
@@ -5,6 +5,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Header from "./components/header/index";
 import Footer from "./components/footer/index";
 import Carrousel from "./components/carousel";
+import $ from 'jquery';
 import Register from "./pages/register";
 import Home from "./pages/home";
 import Login from "./pages/login";
@@ -13,7 +14,34 @@ import { AuthUserKeepLogin } from "./redux/action/auth-action";
 
 function Main() {
   const dispatch = useDispatch();
-  useEffect(() => dispatch(AuthUserKeepLogin()), []);
+  useEffect(() =>  {
+
+    $(".dropdown").on('click', function() {
+      let dropdown_open = $('.dropdown.open');
+      if (!$(this).hasClass('open')) {
+        if (dropdown_open) {
+          $('.dropdown-menu', dropdown_open).stop(true, true).slideUp("fast");
+          $('.dropdown.open').removeClass('open')
+        }
+  
+        $('.dropdown-menu', this).stop(true, true).slideDown("fast");
+        $(this).addClass('open');
+      } else {
+        $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+        $(this).removeClass('open');
+      }
+    });
+    $(document).on('click', function(e) {
+      let dropdown_open = $('.dropdown.open');
+      if (!dropdown_open.is(e.target) && !dropdown_open.has(e.target).length) {
+        $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+        $('.dropdown.open').removeClass('open')
+      }
+    });
+
+    dispatch(AuthUserKeepLogin())
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
